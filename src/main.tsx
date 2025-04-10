833eb3cead9665c98ecbe1e5e90a052205d148cb
Shyam-SS <141055908+Shyam-SS@users.noreply.github.com>
2025-04-10 13:11:32 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import './index.css';
-import App from './App.tsx';
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <NextThemesProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <App />
+      </NextThemesProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
